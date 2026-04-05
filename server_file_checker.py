55b36cf48daf23bc5705e333f5b4a0d5efec404c
FedeJack06 <giacobbefederico6@gmail.com>
2026-04-06 00:59:27 +0200

    ADD script to check file sent to server from the station * find if new file exists * convert file to TOA5

---
+++ server_file_checker.py
@@ -0,0 +1,26 @@
+from tob3_py_converter.src import tob3_to_toa5 as tt
+from src import pre_processing as pp
+
+from pathlib import Path
+
+path = Path("./data")
+stringa_da_cercare = "Slow_2026-03-26_14"
+
+# Usa rglob per cercare in questa cartella E in tutte le sottocartelle
+# Usa glob se vuoi cercare SOLO nella cartella principale
+file_trovati = path.glob(f"*{stringa_da_cercare}*") #.rglob
+
+# file_trovati è un generatore, possiamo iterarlo
+trovato_almeno_uno = False
+for file in file_trovati:
+    if file.is_file(): # Assicuriamoci che sia un file e non una cartella
+        print(f"Find: {file}")
+        trovato_almeno_uno = True
+        result = tt.tob3toa5(file_path=file, out_dir=path)
+        print(result)
+
+if not trovato_almeno_uno:
+    print("Nessun file trovato con quella stringa.")
+
+
+
