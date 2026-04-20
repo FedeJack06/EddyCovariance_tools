e2ef68f957125ec1a77480d5d41cba0194d34b58
FedeJack06 <giacobbefederico6@gmail.com>
2026-04-20 20:14:45 +0200

    all src function with description and comment

--- src/db_tools.py
+++ src/db_tools.py
@@ -1,29 +1,54 @@
 import logging
 import duckdb as db
 import pandas as pd
+from typing import Dict, List
 from .config import InputFileConfig, StationConfig
 from .file_manager import get_files_from_station, toa5_to_df
+from .pre_processing import filter_df_toa5
 
 logger = logging.getLogger(__name__)
 
-def df_to_db(con: db.DuckDBPyConnection, df_in: pd.DataFrame, table, map_df_db_cols):
+def df_to_db(con: db.DuckDBPyConnection, 
+             df_in: pd.DataFrame, 
+             db_table: str, 
+             map_df_db_cols: Dict[str, str]):
     """
+    Fill a database table with a Pandas dataframe.
+    You must specify a relation between the column names 
+    in the dataframe and the column names in the table.
+
     Parameters
     ----------
+    con: db.DuckDBPyConnection
+        connection to the database
+    df_in: pd.DataFrame
+        the Pandas dataframe to put into the table
+    db_table: str
+        the name of the table to fill
+    map_df_db_cols: Dict[str, str]
+        a map that links the column names in the dataframe (as keys)
+        to the column names in the table (as value)
+
     Returns
     ----------
+    Number of rows inserted
     """
+
+    #put the column name in the dataframe and in the table in two list
+    #same list index identifies a pair
     table_cols = []
     df_cols = []
     for key, value in map_df_db_cols.items():
         df_cols.append(key)
         table_cols.append(value)
 
+    #query to insert dataframe rows into a table
     query = f"""
-        INSERT INTO {table} ({", ".join(table_cols)})
+        INSERT INTO {db_table} ({", ".join(table_cols)})
         SELECT {", ".join(df_cols)} FROM df_in
     """
     try:
+        #get the number of inserted rows
         inserted_rows = con.execute(query).fetchone()[0]
         return inserted_rows
     except db.Error as e:
@@ -31,30 +56,70 @@ def df_to_db(con: db.DuckDBPyConnection, df_in: pd.DataFrame, table, map_df_db_c
         # Questa è un'ottima pratica ("don't be overly defensive").
         raise
 
-def fill_db_files_list(con: db.DuckDBPyConnection, db_table, file_list, config: InputFileConfig) -> None:
+def fill_db_toa5(con: db.DuckDBPyConnection, 
+                 db_table: str, 
+                 file_list: List, 
+                 config: InputFileConfig) -> None:
     """
-    Reads raw data files, renames columns according to configuration, 
-    and uploads them to the DuckDB connection.
+    Fills a database table with a list of TOA5 files.
+
+    Parameters
+    ----------
+    con: db.DuckDBPyConnection
+        connection to the database
+    db_table: str
+        the name of the table to fill
+    file_list: List
+        a list containing the files to put into the database table
+    config: InputFileConfig
+        configuration file input, with all the info about the 
+        columns to be inported.
     """
     for file in file_list:
-
         df, header = toa5_to_df(file, config=config)
+        
+        #remove 999.99 numbers, replaced with NAN
+        df = filter_df_toa5(df)
 
-        numeric_cols = df.select_dtypes(include=['number']).columns
-        df[numeric_cols] = df[numeric_cols].mask(df[numeric_cols] > 999)
-
+        #get the dictiocary that link the column name in the dataframe
+        #and the column name in the table
         map = config.get_table_cols_name()
 
         try:
-            insert_query = df_to_db(con=con, df_in=df, table=db_table, map_df_db_cols=map)
+            #insert entire dataframe into the table
+            insert_query = df_to_db(con=con, df_in=df, db_table=db_table, map_df_db_cols=map)
             logging.info(f"{file}: Query OK, {insert_query} row(s) affected")
         except db.Error as e:
             logging.error(f"Error importing file: {file} | Error: {e}")
 
-def fill_station_db(con: db.DuckDBPyConnection, station: StationConfig, input_dir):
+def fill_db_station_toa5(con: db.DuckDBPyConnection, 
+                    station: StationConfig, 
+                    input_dir: str) -> None:
+    """
+    Fills the database with all the files produced by a station.
+    StationConfig object is used to put different file types into
+    the correct database tables. A table corresponds to a file type,
+    and it is filled only with files of the same type.
+    Different type of files can be in the same directory. They are sorted
+    based on the substring in their name, specified in the station configuration.
+
+    Parameters
+    ----------
+    con: db.DuckDBPyConnection
+        connection to the database
+    station: StationConfig
+        Station object conaining all the input file configuration
+    input_dir: str
+        Directory containing all the files produced by the station.
+    """
+    #get a dict with the input configuration id as key and
+    #the list of related files as value
     all_station_files = get_files_from_station(folder_path = input_dir, station=station)
 
     for config_id, file_list in all_station_files.items():
+        #get the InputFileConfig object based on its id
         config = station.get_input_config(config_id)
+        #get the database table name associated with this input config
         table_name = station.get_table_name(config_id)
-        fill_db_files_list(con=con, db_table=table_name, file_list=file_list, config=config)
+        #fill the table with files of the same type
+        fill_db_toa5(con=con, db_table=table_name, file_list=file_list, config=config)
--- src/file_manager.py
+++ src/file_manager.py
@@ -1,121 +1,222 @@
 import os
 import logging
-from .config import InputFileConfig, StationConfig
 from typing import List, Dict, Tuple
 import pandas as pd
-import warnings
+from .config import InputFileConfig, StationConfig
 
 logger = logging.getLogger(__name__)
 
-import os
-
 def toa5_to_df(input_file : str,
-                config: InputFileConfig
-            ) -> Tuple[pd.DataFrame, List[str]]:
+               config: InputFileConfig,
+               date_index: bool = False) -> Tuple[pd.DataFrame, List[str]]:
     """
-    Work with TOA5 file data from Campbell dataloggers (Sonic, Slow, etc.)
-    with Sonic Anemometer and Termoigrometers with N vertical levels
+    Import a TOA5 Campbell file into a Pandas Dataframe.
+    TOA5 should have the datetime column named "TIMESTAMP", important only
+    if you chose to set the datetime column as index.
+    The output dataframe has all the columns and types specified in the
+    InputFileConfig passed. Be sure you have set the config right.
+    The name of the columns in the dataframe are the same of the files.
 
     Parameters
     ----------
     input_file: str
-        path to file in TOA5
+        path of TOA5 file
     config: InputFileConfig
-        configuration file input
+        configuration file input, with all the info about the 
+        columns to be inported.
+    date_index: bool
+        If true sets the datetime column ("TIMESTAMP") as index and
+        removes it from df columns. Default False, so keeps the 
+        default numeric index.
+
     Returns
     -------
     Tuple[pd.DataFrame, List[str]]
         - The file as a dataframe.
         - List of strings containing the original 4 header lines.
     """
-    
-    #extract header
+
+    #extract 4 line header from file
     raw_header = []
     with open(input_file, 'r', encoding='utf-8') as f:
         for _ in range(4):
             raw_header.append(f.readline())
 
-    #csv to df
+    #get pandas dtype for each column from the config object
     types = config.get_file_cols_type()
+    #toa5 file to pandas 
     df = pd.read_csv(input_file, sep=",", header=1, skiprows=[2, 3], dtype="string")
+    #remove column not specified in the input files configuration
     df = df.filter(items=config.get_file_cols_name())
+    #convert the column dtypes to dtypes specified in the config
     df = df.astype(dtype=types, errors='raise')
+
+    #set datetime column as index
+    if date_index:
+        #column_name_date = df.dtypes[df.dtypes == 'datetime64[ms]'].index[0]
+        df.set_index("TIMESTAMP", inplace=True)
+        df.drop(columns=["TIMESTAMP"], inplace=True, errors='ignore')
 
     return df, raw_header
 
-def get_files_list(folder_path, start_name, end_name):
+def csv_to_df(input_file : str,
+               config: InputFileConfig,
+               index: str = None) -> pd.DataFrame:
     """
-    Get a sorted list of files in a folder with their full path.
+    Import a generic csv file into a Pandas Dataframe.
+    The input file must have the first row containing the name of
+    th columns.
+    The output dataframe has all the columns and types specified in the
+    InputFileConfig passed. Be sure you have set the config right.
+    The name of the columns in the dataframe are the same of the files.
+    You can specify the column to set as dataframe index.
 
     Parameters
     ----------
-    folder_path
+    input_file: str
+        path of csv file
+    config: InputFileConfig
+        configuration file input, with all the info about the 
+        columns to be inported.
+    index: str
+        The name of the column to set as dataframe index.
+        This column will be removed from the columns of the dataframe.
+        Default None, so keeps the default numeric index.
+
+    Returns
+    -------
+    pd.DataFrame
+        Dataframe containing the columns choosen in InputConfigFile.
+    """
+
+    #get pandas dtype for each column from the config object
+    types = config.get_file_cols_type()
+
+    df = pd.read_csv(input_file, sep=",", header=0, dtype="string")
+    #remove column not specified in the input files configuration
+    df = df.filter(items=config.get_file_cols_name())
+    #convert the column dtypes to dtypes specified in the config
+    df = df.astype(dtype=types, errors='raise')
+
+    #set datetime column as index
+    if index:
+        #column_name_date = df.dtypes[df.dtypes == 'datetime64[ms]'].index[0]
+        df.set_index(index, inplace=True)
+        df.drop(columns=[index], inplace=True, errors='ignore')
+
+    return df
+
+def get_files(folder_path: str,
+              start_name: str, 
+              end_name: str) -> List:
+    """
+    Get a sorted list of files from a folder. The name of the files
+    must start with "start_name" and end with "end_name".
+    The search is case-sensitive.
+    The files are returned as String with their relative path.
+
+    Parameters
+    ----------
+    folder_path: str
         Search is perfomed in this path.
-    start_name
-        Prefix of the filename to be selected.
-    end_name
-        Suffix of the filename to be selected.
+    start_name: str
+        First part of the filename to be selected.
+    end_name: str
+        Final part of the filename to be selected.
+
+    Returns
+    -------
+    List[str]
+        A list of the selected files with their relative path.
     """
     try:
-        # Usa os.path.join per unire folder_path e f
+        # get a sorted list of files that start and end with the parameters
         files = sorted([
             os.path.join(folder_path, f) 
             for f in os.listdir(folder_path)
             if f.startswith(start_name) and f.endswith(end_name)
         ])
-        logger.info(f"Find {len(files)} file {start_name}... in {folder_path}.")
+        # how many files found
+        logger.info(f"Find {len(files)} file starting with {start_name} and ending \
+                    with {end_name}, in {folder_path}.")
         return files
     except FileNotFoundError:
         logger.error(f"Error: folder {folder_path} not found.")
         return []
     
-def get_files_from_config(folder_path, 
-                          input_config: InputFileConfig
-                        ) -> List:
+def get_files_sub(folder_path: str,
+                  substring: str) -> List:
     """
-    Get a sorted list of files related to an ohject InputFileConfig.
+    Get a sorted list of files from a folder. The name of the files
+    must contains the substring passed.
+    The search is case-insensitive.
+    The files are returned as String with their relative path.
 
     Parameters
     ----------
-    folder_path
+    folder_path: str
         Serach is perfomed in this path.
-    input_config: InputFileConfig
-        Configuration object conaining the identifier of the files.
-        Only files containing the configuration name_file attribute in the filename are selected.
-        Serch string case-insensitive
+    substring: str
+        Substring contained in the name of the searched files.
+
+    Returns
+    -------
+    List[str]
+        A list of the selected files with their relative path.
     """
-    if not isinstance(input_config, InputFileConfig):
-        raise TypeError("input_config must be an instance of InputFileConfig dataclass")
-    
-    substring = input_config.file_name
+
     try:
+        # get a sorted list of files that contains the substring in their name
         files = sorted([
             os.path.join(folder_path, f)
             for f in os.listdir(folder_path) 
             if substring.lower() in f.lower()
         ])
+        # how many files found
         logger.info(f"Find {len(files)} file *{substring}*... in {folder_path}.")
         return files
     except FileNotFoundError:
         logger.error(f"Error: folder {folder_path} not found.")
         return []
     
-def get_files_from_station(folder_path, 
-                           station: StationConfig
-                        ) -> Dict[str, List]:
+def get_files_from_station(folder_path: str, 
+                           station: StationConfig) -> Dict[str, List]:
     """
-    Return a dictionary of list of files. 
-    Key is the config ID, value is the related list of input files.
+    Gets all the files related to one station. One search is performed
+    for each InputFileConfig object in the StationConfig.
+    Returns a dictionary, with a list of files for each 
+    InputFileConfig of the station.
+    The files are returned as String with their relative path.
+
+    Parameters
+    ----------
+    folder_path: str
+        Serach is perfomed in this path.
+    station: StationConfig
+        Station object conaining all the input file configuration.
+        Only files containing the "input_files_name" in the filename are selected.
+        Serch string case-insensitive
+
+    Returns
+    ----------
+    Dict[str, List]
+        Return a dictionary of list of files. 
+        Key is the config ID, value is the related list of input files,
+        with their relative path.
 
     """
     if not isinstance(station, StationConfig):
         raise TypeError("station must be an instance of StationConfig dataclass")
     
+    #dictionary: key is the config_id, value is the related list of files
     dict_files = {}
 
-    configs = station.get_files_config()
-    for config_id, config in configs.items():
-        list_of_file = get_files_from_config(folder_path=folder_path, input_config=config)
+    #get all the substrings to search files
+    files_substring = station.input_files_name
+    for config_id, substring in files_substring.items():
+        #get the list of files of the same type (the same input config)
+        list_of_file = get_files_sub(folder_path=folder_path, substring=substring)
+        #output
         dict_files[config_id] = list_of_file
 
     return dict_files
